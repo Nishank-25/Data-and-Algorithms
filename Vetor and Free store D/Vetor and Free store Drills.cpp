73569b92a9d8590d4b7c7167e7d70e9bf75d9d99
Nishank Patil <NishankPatil25@outlook.com>
2020-12-24 12:49:14 +0530

    Int added

--- Vetor and Free store D/Vetor and Free store Drills.cpp
+++ Vetor and Free store D/Vetor and Free store Drills.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int i = 54;
+	cout << "saale tp qa krra";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
